bfb4f806e7696a1ed92681e87f7e01cc3056b87b
Sesgey Lebed <swansergey7@gmail.com>
2023-11-02 19:12:02 +0800

    add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+
+// styles
+import './Navbar.css'
+
+
+export default function Navbar() {
+
+  return (
+    <div className="navbar">
+      <nav>    
+        <Link to="/" className="brand">
+          <h1>
+            Master Recipe
+          </h1>
+        </Link>
+        <Link to="/create">Create Recipe</Link>
+      </nav>
+    </div>
+  )
+}
