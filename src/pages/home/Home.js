00456dec9b5cd81770f87e161dfe980877805042
Sesgey Lebed <swansergey7@gmail.com>
2023-11-02 20:24:39 +0800

    add fetching

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,21 @@
 import "./Home.css";
+import { useFetch } from '../../hooks/useFetch';
+
+
 
 
 
 export default function Home() {
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes")
+
+
   return (
-    <div>
-      Home
+    <div className="home">
+      {error && <p className="error ">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.map(recipes => (
+        <h2 key={recipes.id}>{recipes.title}</h2>
+      ))}
     </div>
   )
 }
