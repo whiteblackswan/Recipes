45edb62a389932610fc370283edfe234a40f2ff2
Sesgey Lebed <swansergey7@gmail.com>
2023-11-18 15:27:20 +0800

    add the navbar a color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,16 @@ import { useTheme } from '../hooks/useTheme'
 
 // styles
 import './Navbar.css'
-
-
-// components
 import Searchbar from './Searchbar'
 
-
-
 export default function Navbar() {
-  const { color } = useTheme()
-
-
+  const { color, changeColor } = useTheme()
 
   return (
-    <div className="navbar" style={{background: color}}>
-      <nav>    
+    <div className="navbar" style={{ background: color }}>
+      <nav onClick={() => changeColor('green')}>
         <Link to="/" className="brand">
-          <h1>
-            Master Recipe
-          </h1>
+          <h1>Cooking Ninja</h1>
         </Link>
         <Searchbar />
         <Link to="/create">Create Recipe</Link>
