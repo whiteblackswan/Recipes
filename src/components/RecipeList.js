e93d83f35b2c24aec3407bc14724a637c8062ba0
Sesgey Lebed <swansergey7@gmail.com>
2023-11-20 09:15:06 +0800

    add realtime collection data

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,11 +1,10 @@
-import { useTheme } from '../hooks/useTheme';
-import { Link } from 'react-router-dom';
-import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+import Trashcan from '../assets/trashcan.svg'
+import { projectFirestore } from "../firebase/config"
 
 // styles
-import './RecipeList.css';
-
+import './RecipeList.css'
 
 export default function RecipeList({ recipes }) {
   const { mode } = useTheme()
@@ -14,18 +13,25 @@ export default function RecipeList({ recipes }) {
     return <div className="error">No recipes to load...</div>
   }
 
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete()
+  }
 
   return (
-    <div className='recipe-list'>
-        {recipes.map(recipe => (
-            <div key={recipe.id} className={ `card ${mode}`}>
-                <h3>{recipe.title}</h3>
-                <p>{recipe.cookingTime} to make.</p>
-                <div>{recipe.method.substring(0, 100)}...</div>
-                <Link to={`/recipes/${recipe.id}`}>Cook this</Link>    
-            </div>
-        ))}
+    <div className="recipe-list">
+      {recipes.map(recipe => (
+        <div key={recipe.id} className={`card ${mode}`}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make.</p>
+          <div>{recipe.method.substring(0, 100)}...</div>
+          <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+          <img 
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+            src={Trashcan} alt="delete icon" 
+          />
+        </div>
+      ))}
     </div>
   )
 }
-
