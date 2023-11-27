f7f2c0b3cd637693e15b12d4a7b242b81402f8ee
Sergey Lebed <swansergey7@gmail.com>
2023-11-27 15:28:36 +0200

    build

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { projectFirestore } from '../../firebase/config'
 import { useNavigate } from 'react-router-dom'
 
